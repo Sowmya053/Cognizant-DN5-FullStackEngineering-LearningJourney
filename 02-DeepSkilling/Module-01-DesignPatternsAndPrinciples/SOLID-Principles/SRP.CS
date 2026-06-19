d4c92401afea5c4916f9f47c1c0e5ed026c61b88
Sowmya053 <vusowmya07@gmail.com>
2026-06-19 11:21:36 +0530

    Added SRP example to root SOLID folder

---
+++ 02-DeepSkilling/Module-01-DesignPatternsAndPrinciples/SOLID-Principles/SRP.CS
@@ -0,0 +1,17 @@
+using System;
+
+class Invoice
+{
+    public void CalculateTotal()
+    {
+        Console.WriteLine("Calculating Total");
+    }
+}
+
+class InvoicePrinter
+{
+    public void Print()
+    {
+        Console.WriteLine("Printing Invoice");
+    }
+}
