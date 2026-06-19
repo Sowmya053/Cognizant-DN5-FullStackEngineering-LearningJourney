1c48e74b882d14efc7c390bdf52ec80c981bb5f1
Sowmya053 <vusowmya07@gmail.com>
2026-06-19 11:09:09 +0530

    Added Singleton Pattern

---
+++ 02-DeepSkilling/Module-01-DesignPatternsAndPrinciples/Creational-Patterns/Singleton.cs
@@ -0,0 +1,20 @@
+using System;
+
+class Singleton
+{
+    private static Singleton instance;
+
+    private Singleton()
+    {
+    }
+
+    public static Singleton GetInstance()
+    {
+        if(instance == null)
+        {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
