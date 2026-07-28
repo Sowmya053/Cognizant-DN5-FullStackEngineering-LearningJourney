4719cea7e99315fee23e1784691fa0f89a690982
sowmya <vusowmya07@gmail.com>
2026-07-28 19:04:18 +0530

    Added Module-07 Microservices

---
+++ deepskilling/Module-07-Microservices/JWTAuthentication/JWTAuthenticationAPI/Controllers/SecureController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWTAuthenticationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Welcome! JWT Authentication Successful.");
+        }
+    }
+}
